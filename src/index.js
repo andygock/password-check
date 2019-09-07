19e9a92378aab7e53c5ca5a4220de2044e0e5849
Andy Gock <andy@gock.net>
2019-09-08 04:47:44 +1000

    Improve styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+import "../node_modules/purecss/build/pure.css";
 import "./styles.css";
 
 // https://haveibeenpwned.com/API/v3#PwnedPasswords
@@ -22,8 +23,13 @@ const digest = async (input, type = "SHA-1") => {
 };
 
 const fetchPwned = async hash => {
+  // console.log(`fetchPwned('${hash}')`);
+
   const url = `https://api.pwnedpasswords.com/range/${hash.slice(0, 5)}`;
-  const response = await axios.get(url);
+  const response = await axios({
+    method: "get",
+    url
+  });
 
   if (response.status !== 200) {
     throw new Error("API server not responded unexpectedly");
@@ -57,57 +63,81 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>Password Check</h1>
-      <p>
-        Check a password against known data breaches using{" "}
-        <a href="https://haveibeenpwned.com/API/v3#PwnedPasswords">
-          Have I Been Pwned API
-        </a>
-        .
-      </p>
-      <input
-        ref={inputRef}
-        type="password"
-        value={password}
-        placeholder="Enter password..."
-        onChange={e => setPassword(e.target.value)}
-        onKeyUp={e => {
-          if (e.key === "Enter") {
-            search(password);
-            e.target.select();
-          }
-        }}
-      />
-      <button
-        type="submit"
-        onClick={e => {
-          search(password);
-          inputRef.current.select();
-        }}
-        disabled={isSearching}
-      >
-        Search
-      </button>
-      <p style={{ color: count > 0 ? "red" : "black" }}>
-        This password has been seen {count} time{count !== 1 ? "s" : ""} before.
-      </p>
-      {count > 0 && (
-        <p style={{ color: "red" }}>
-          This password has previously appeared in a data breach and should
-          never be used. If you've ever used it before anywhere, change
-          it immediately!
-        </p>
-      )}
-      <p>
-        Password complexity is <strong>not</strong> checked.
-      </p>
-      <footer>
-        &copy; <a href="https://gock.net/">Andy Gock</a> |{" "}
-        <a href="https://github.com/andygock/password-check/">
-          Source code on GitHub
-        </a>
-      </footer>
+    <div className="pure-g">
+      <div className="pure-u-1">
+        <header>
+          <h1>Password Check</h1>
+          <p>
+            Check a password against known data breaches using the{" "}
+            <a href="https://haveibeenpwned.com/API/v3#PwnedPasswords">
+              Have I Been Pwned API
+            </a>
+            .
+          </p>
+        </header>
+
+        <main>
+          <form className="pure-form">
+            <fieldset>
+              <label htmlFor="password">Enter password to check:</label>
+              <input
+                id="password"
+                ref={inputRef}
+                type="password"
+                value={password}
+                placeholder="Enter password..."
+                onChange={e => setPassword(e.target.value)}
+                onKeyUp={e => {
+                  if (e.key === "Enter") {
+                    search(password);
+                    e.target.select();
+                  }
+                }}
+              />
+              <button
+                className="pure-button pure-button-primary"
+                type="submit"
+                onClick={e => {
+                  e.preventDefault();
+                  search(password);
+                  inputRef.current.select();
+                }}
+                disabled={isSearching}
+              >
+                Search
+              </button>
+            </fieldset>
+          </form>
+          <p style={{ color: count > 0 ? "red" : "black" }}>
+            This password has been seen <strong>{count}</strong> time
+            {count !== 1 ? "s" : ""} before.
+          </p>
+          {count > 0 && (
+            <p style={{ color: "red" }}>
+              This password has previously appeared in a data breach and should
+              never be used. If you've ever used it before anywhere, change it
+              immediately!
+            </p>
+          )}
+          <p>
+            Password complexity is <strong>not</strong> checked.
+          </p>
+          <p>
+            This page will not send your password away from the web browser. The
+            first 5 characters of the password's SHA-1 hash is calculated
+            in-browser and sent to{" "}
+            <a href="https://haveibeenpwned.com/API/v3#PwnedPasswords">
+              https://haveibeenpwned.com/API/v3
+            </a>
+          </p>
+        </main>
+        <footer>
+          &copy; <a href="https://gock.net">Andy Gock</a> |{" "}
+          <a href="https://github.com/andygock/password-check/">
+            Source code on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
